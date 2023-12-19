2499b79476a32adb468fd8dda99e5d5f2d04461c
Tri Nguyen <trinm.dev.1112@gmail.com>
2023-12-19 16:59:23 +0700

    improve lighthouse

--- src/components/hero-banner/index.js
+++ src/components/hero-banner/index.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 // import { Swiper, SwiperSlide } from "swiper/react";
 // import { EffectFade } from "swiper";
 // internal
-import slider_img_1 from "@assets/img/slider/13/slider-1.png";
+import slider_img_1 from "@assets/img/slider/13/slider-1.webp";
 // import slider_img_2 from "@assets/img/slider/13/slider-1.png";
 // import slider_img_3 from "@assets/img/slider/13/slider-1.png";
 import { RightArrow } from "@svg/index";
@@ -103,13 +103,13 @@ const HeroBanner = () => {
                   <div className="slider__thumb-13 text-end mr-40">
                     <span className="slider__thumb-13-circle-1"></span>
                     <span className="slider__thumb-13-circle-2"></span>
-                    {/* <Image
+                    <Image
                       // src={item.img}
                       src={slider_img_1}
                       alt="slider img"
                       priority={true}
                       className="position-relative z-1"
-                    /> */}
+                    />
                   </div>
                 </div>
               </div>
