f4123b7ff3de8846b2abf1f73c2ad73cdfc104bc
Tri Nguyen <trinm.dev.1112@gmail.com>
2023-12-19 11:24:28 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,29 @@
+const bundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.tringuyenminh.com',
+        pathname: '**',
+      }
+    ],
+  },
+}
+
+module.exports = bundleAnalyzer(nextConfig)
