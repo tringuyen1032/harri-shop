012dec6ba718dec78bb23ae0612cff95b778f3c8
Tri Nguyen <trinm.dev.1112@gmail.com>
2023-12-27 10:08:43 +0700

    fix bug

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -1,5 +1,5 @@
 import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const notifySuccess = (message) =>
   toast.success(message, {
